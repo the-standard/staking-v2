5ea6cc467fa59a774ef80405da0082d73bbff7b9
Ewan Sheldon <ewansheldon@gmail.com>
2024-10-23 15:44:24 +0200

    test reset of position start on decrease

--- test/stakingTST.js
+++ test/stakingTST.js
@@ -334,56 +334,36 @@ describe('StakingTST', async () => {
   describe('decreaseStake', async() => {
     it('allows users to remove part or all of their stake', async () => {
       const tstStake = ethers.utils.parseEther('10000');
-      const eurosStake = ethers.utils.parseEther('100');
       await TST.mint(user1.address, tstStake);
-      await EUROs.mint(user1.address, eurosStake);
       await TST.connect(user1).approve(Staking.address, tstStake);
-      await EUROs.connect(user1).approve(Staking.address, eurosStake);
-      await Staking.connect(user1).increaseStake(tstStake, eurosStake);
+      await Staking.connect(user1).increaseStake(tstStake);
 
       let position = await Staking.positions(user1.address);
       expect(position.TST).to.equal(tstStake);
-      expect(position.EUROs).to.equal(eurosStake);
 
-      await expect(Staking.connect(user1).decreaseStake(tstStake.add(1), 0)).to.be.revertedWithCustomError(Staking, 'InvalidRequest');
-      await expect(Staking.connect(user1).decreaseStake(0, eurosStake.add(1))).to.be.revertedWithCustomError(Staking, 'InvalidRequest');
+      await expect(Staking.connect(user1).decreaseStake(tstStake.add(1))).to.be.revertedWithCustomError(Staking, 'InvalidRequest');
 
-      await expect(Staking.connect(user1).decreaseStake(tstStake.div(2), 0)).not.to.be.reverted;
+      await expect(Staking.connect(user1).decreaseStake(tstStake)).not.to.be.reverted;
 
-      position = await Staking.positions(user1.address);
-      expect(position.TST).to.equal(tstStake.div(2));
-      expect(position.EUROs).to.equal(eurosStake);
-
-      expect(await TST.balanceOf(Staking.address)).to.equal(tstStake.div(2));
-      expect(await TST.balanceOf(user1.address)).to.equal(tstStake.div(2));
-
-      await expect(Staking.connect(user1).decreaseStake(tstStake.div(2), eurosStake)).not.to.be.reverted;
-      
       position = await Staking.positions(user1.address);
       expect(position.TST).to.equal(0);
-      expect(position.EUROs).to.equal(0);
 
       expect(await TST.balanceOf(Staking.address)).to.equal(0);
-      expect(await EUROs.balanceOf(Staking.address)).to.equal(0);
       expect(await TST.balanceOf(user1.address)).to.equal(tstStake);
-      expect(await EUROs.balanceOf(user1.address)).to.equal(eurosStake);
     });
 
     it('resets staking start when stake is decreased, removes when emptied', async () => {
       const tstStake = ethers.utils.parseEther('10000');
-      const eurosStake = ethers.utils.parseEther('100');
       await TST.mint(user1.address, tstStake);
-      await EUROs.mint(user1.address, eurosStake);
       await TST.connect(user1).approve(Staking.address, tstStake);
-      await EUROs.connect(user1).approve(Staking.address, eurosStake);
-      await Staking.connect(user1).increaseStake(tstStake, eurosStake);
+      await Staking.connect(user1).increaseStake(tstStake);
 
-      let decrease = await Staking.connect(user1).decreaseStake(tstStake.div(2), 0)
+      let decrease = await Staking.connect(user1).decreaseStake(tstStake.div(2))
 
       position = await Staking.positions(user1.address);
       expect(position.start).to.equal((await ethers.provider.getBlock(decrease.blockNumber)).timestamp);
 
-      decrease = await Staking.connect(user1).decreaseStake(tstStake.div(2), eurosStake)
+      decrease = await Staking.connect(user1).decreaseStake(tstStake.div(2))
 
       position = await Staking.positions(user1.address);
       expect(position.start).to.equal(0);
